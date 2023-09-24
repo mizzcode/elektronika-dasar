6d7c8a423e660cd4e0239eabe80670c00dde0f7b
Misbahudin <misbahudin.id@gmail.com>
2023-09-24 13:40:26 +0700

    feat: close menu

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     const links = document.getElementById("myLinks");
     const menu = document.getElementById("menu");
+    const nav = document.querySelector("nav");
 
-        if (window.innerWidth <= 426) {
-            links.style.display = "none";
-            menu.innerHTML = "<i class='fa-solid fa-bars' style='color: #ffffff;'></i>";
-            
-            menu.addEventListener("click", function() {
+    if (window.innerWidth <= 426) {
+        nav.style.height = "50px";
+        links.style.display = "none";
+        menu.innerHTML = "<i class='fa-solid fa-bars' style='color: #ffffff;'></i>";
+        
+        menu.addEventListener("click", function() {
+            if (links.style.display === "none") {
                 links.style.display = "flex";
                 document.querySelector("nav").style.height = "auto";
-                menu.innerHTML = "<i class='fa-solid fa-xmark' style='color: #ffffff;''></i>";
-            })
-        } else {
-            links.style.display = "flex";
-            menu.style.display = "none";
-        }
+                menu.innerHTML = "<i class='fa-solid fa-xmark' style='color: #ffffff;'></i>";
+            } else {
+                nav.style.height = "50px";
+                links.style.display = "none";
+                menu.innerHTML = "<i class='fa-solid fa-bars' style='color: #ffffff;'></i>";
+            }
+        });
+    } else {
+        links.style.display = "flex";
+        menu.style.display = "none";
+    }
 });
